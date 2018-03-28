a53d15d0d4cee54afa5a9f230508414546c171f6
pbudzyns <Paakhec@19>
2018-03-29 00:05:16 +0300

    extract picture type var

--- Lab1/recognize_zodiac.py
+++ Lab1/recognize_zodiac.py
@@ -5,17 +5,17 @@ import glob
 
 
 def read_pictures(directory):
+    type = 'bmp'
     pictures, tests = {}, {}
-    filenames = glob.glob(directory + '*.bmp')
+    filenames = glob.glob(directory + '*.' + type)
     for filename in filenames:
-        name = filename.replace(directory, '').replace('.bmp', '')
+        name = filename.replace(directory, '').replace('.' + type, '')
         if 'test' not in filename:
             pictures[name] = misc.imread(filename, mode='L')
         elif 'test' in filename:
             tests[name] = misc.imread(filename, mode='L')
 
     return pictures, tests
-
 
 
 if __name__ == "__main__":
