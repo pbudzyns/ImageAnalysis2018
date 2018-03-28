8d12876aa406880350b649e3db4c8e6af1820d0b
pbudzyns <Paakhec@19>
2018-03-29 00:03:11 +0300

    data set reading function

--- Lab1/recognize_zodiac.py
+++ Lab1/recognize_zodiac.py
@@ -1,6 +1,25 @@
-import scipt
+import scipy
 import numpy as np
+from scipy import misc
+import glob
+
+
+def read_pictures(directory):
+    pictures, tests = {}, {}
+    filenames = glob.glob(directory + '*.bmp')
+    for filename in filenames:
+        name = filename.replace(directory, '').replace('.bmp', '')
+        if 'test' not in filename:
+            pictures[name] = misc.imread(filename, mode='L')
+        elif 'test' in filename:
+            tests[name] = misc.imread(filename, mode='L')
+
+    return pictures, tests
+
 
 
 if __name__ == "__main__":
-    pass
+    pictures, tests = read_pictures('pic/')
+    print(pictures.keys())
+    print(tests.keys())
+    print(np.shape(pictures['aries']))
